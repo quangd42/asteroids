9b6fe11678bfbb9ec5bc5e0a5369bc32602577c2
quangd42 <quangdn42@gmail.com>
2024-09-17 14:08:22 +0700

    boilerplate 2

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ import constants as const
 def main():
     print("Starting asteroids!")
     pygame.init()
+
     print(f"Screen width: {const.SCREEN_WIDTH}")
     print(f"Screen height: {const.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+
+    dt = 0
+    clock = pygame.time.Clock()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
